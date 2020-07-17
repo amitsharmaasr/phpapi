4418d388bea7993be870bef57196b9a7e83be517
amitsharmaasr <amit1414707@gmail.com>
2020-07-17 16:21:38 +0530

    add delete api

---
+++ User/delete.php
@@ -0,0 +1,30 @@
+<?php
+
+include_once '../config/database.php';
+include_once '../objects/user.php';
+ 
+$database = new Database();
+$db = $database->getConnection();
+ 
+
+$user = new User($db);
+
+$user->id = isset($_GET['id']) ? $_GET['id'] : die("user id is required");
+
+if($user->delete()){
+    
+    $user_arr=array(
+        "status" => true,
+        "message" => "user deleted!",
+    );
+}
+else{
+    $user_arr=array(
+        "status" => false,
+        "message" => "user couldn't be deleted!",
+    );
+}
+
+print_r(json_encode($user_arr));
+
+?>
