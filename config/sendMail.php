6f3c3105fd2d84b2575ccfe841bc76b3c8a8e198
amitsharmaasr <amit1414707@gmail.com>
2020-07-23 14:08:01 +0530

    add send

--- config/sendMail.php
+++ config/sendMail.php
@@ -12,50 +12,50 @@ class SendMail{
  
     private $username = "postgres";
     private $password = "amit";
+    private $mail;
 
     public  $to;
     public  $subject;
     public  $body;
     
     public function __construct(){
+        $this->mail = new PHPMailer(true);
         ini_set('display_errors', 1);
         error_reporting(E_ALL);
     }
 
     public function send(){
 
-        $mail = new PHPMailer(true);
-
         try {
             //Server settings
-            $mail->SMTPDebug = SMTP::DEBUG_SERVER;                      // Enable verbose debug output
-            $mail->isSMTP();                                            // Send using SMTP
-            $mail->Host       = 'smtp.gmail.com';                       // Set the SMTP server to send through
-            $mail->SMTPAuth   = true;                                   // Enable SMTP authentication
-            $mail->Username   = $this->username;                                // SMTP username
-            $mail->Password   = $this->password;                              // SMTP password
-            $mail->SMTPSecure = 'tls';         				// Enable TLS encryption; `PHPMailer::ENCRYPTION_SMTPS` encouraged
-            $mail->Port       = 587;                                    // TCP port to connect to, use 465 for `PHPMailer::ENCRYPTION_SMTPS` above
+            $this->mail->SMTPDebug = SMTP::DEBUG_SERVER;                      // Enable verbose debug output
+            $this->mail->isSMTP();                                            // Send using SMTP
+            $this->mail->Host       = 'smtp.gmail.com';                       // Set the SMTP server to send through
+            $this->mail->SMTPAuth   = true;                                   // Enable SMTP authentication
+            $this->mail->Username   = $this->username;                                // SMTP username
+            $this->mail->Password   = $this->password;                              // SMTP password
+            $this->mail->SMTPSecure = 'tls';         				// Enable TLS encryption; `PHPMailer::ENCRYPTION_SMTPS` encouraged
+            $this->mail->Port       = 587;                                    // TCP port to connect to, use 465 for `PHPMailer::ENCRYPTION_SMTPS` above
 
             //Recipients
-            $mail->setFrom($this->to);
-            $mail->addAddress($this->to);     // Add a recipient
+            $this->mail->setFrom($this->to);
+            $this->mail->addAddress($this->to);     // Add a recipient
 
-            //$mail->addReplyTo('info@example.com', 'Information');
-            //$mail->addCC('cc@example.com');
-            //$mail->addBCC('bcc@example.com');
+            //$this->mail->addReplyTo('info@example.com', 'Information');
+            //$this->mail->addCC('cc@example.com');
+            //$this->mail->addBCC('bcc@example.com');
 
             // Attachments
-            //$mail->addAttachment('/var/tmp/file.tar.gz');         // Add attachments
-            //$mail->addAttachment('/tmp/image.jpg', 'new.jpg');    // Optional name
+            //$this->mail->addAttachment('/var/tmp/file.tar.gz');         // Add attachments
+            //$this->mail->addAttachment('/tmp/image.jpg', 'new.jpg');    // Optional name
 
             // Content
-            $mail->isHTML(true);                                  // Set email format to HTML
-            $mail->Subject = $this->subject;
-            $mail->Body    = $this->body;
-            //$mail->AltBody = 'This is the body in plain text for non-HTML mail clients';
+            $this->mail->isHTML(true);                                  // Set email format to HTML
+            $this->mail->Subject = $this->subject;
+            $this->mail->Body    = $this->body;
+            //$this->mail->AltBody = 'This is the body in plain text for non-HTML mail clients';
 
-            $mail->send();
+            $this->mail->send();
             
 	        return true;
         } catch (Exception $e) {
